e9e8c4ca7869ea492abe4754aaf0b0d17f60c913
elikemdanielsdev <elikemdaniels@gmail.com>
2024-11-09 18:30:04 +0000

    Configured Sanity image

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
+import { urlFor } from "@/sanity/lib/image";
 import Link from "next/link";
 
 export default async function Page({
@@ -19,6 +20,20 @@ export default async function Page({
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
+      {post?.mainImage ? (
+        <img
+          className="aspect-[800/300] w-full"
+          src={urlFor(post.mainImage)
+            .width(800)
+            .height(300)
+            .quality(80)
+            .auto("format")
+            .url()}
+          alt={post?.mainImage?.alt || ""}
+          width="800"
+          height="300"
+        />
+      ) : null}
       <h1 className="text-balance text-4xl font-bold">{post?.title}</h1>
       <hr />
       <Link href="/posts">&larr; Return to index</Link>
