b6f6311a295367bc9459cb96da0abd6fa363cdeb
elikemdanielsdev <elikemdaniels@gmail.com>
2024-11-09 14:48:23 +0000

    Updated index page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+import Link from "next/link";
+
+export default async function Page() {
+  return (
+    <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
+      <h1 className="text-4xl font-bold">Home</h1>
+      <hr />
+      <Link href="/posts">Posts index &rarr;</Link>
+    </section>
+  );
 }
