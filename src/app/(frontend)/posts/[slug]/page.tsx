c8b3b60f73e6e7b2e76f6512d47b1c0836c209c9
elikemdanielsdev <elikemdaniels@gmail.com>
2024-11-10 15:45:31 +0000

    Added single post

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,11 +1,7 @@
 import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
-import { urlFor } from "@/sanity/lib/image";
-import { PortableText } from "next-sanity";
-import Link from "next/link";
-import Image from "next/image";
-import { components } from "@/sanity/portableTextComponents";
+import { Post } from "@/components/post";
 
 export default async function Page({
   params,
@@ -23,29 +19,7 @@ export default async function Page({
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      {post?.mainImage ? (
-        <Image
-          className="aspect-[800/300] w-full"
-          src={urlFor(post.mainImage)
-            .width(800)
-            .height(300)
-            .quality(80)
-            .auto("format")
-            .url()}
-          alt={post?.mainImage?.alt || ""}
-          width="800"
-          height="300"
-        />
-      ) : null}
-      <h1 className="text-balance text-4xl font-bold">{post?.title}</h1>
-
-      {post?.body ? (
-        <div className="prose">
-          <PortableText value={post?.body} components={components} />
-        </div>
-      ) : null}
-      <hr />
-      <Link href="/posts">&larr; Return to index</Link>
+      <Post {...post} />
     </main>
   );
 }
