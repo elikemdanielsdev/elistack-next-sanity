6999daa655060528759fe80e9844ff9f27a627b9
elikemdanielsdev <elikemdaniels@gmail.com>
2024-11-11 08:55:08 +0000

    General update of types

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,10 +1,10 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
 
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
+import { blockContentType } from "@/sanity/schemaTypes/documents/blockContentType";
+import { categoryType } from "@/sanity/schemaTypes/documents/categoryType";
+import { postType } from "@/sanity/schemaTypes/documents/postType";
+import { authorType } from "@/sanity/schemaTypes/documents/authorType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [blockContentType, categoryType, postType, authorType],
-}
+};
